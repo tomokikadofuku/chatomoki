9e5f7eba4770bb7507735177e156409ae607d258
tomokikadofuku <gtomo0917@gmail.com>
2018-09-05 15:19:42 +0900

    変更済み

---
+++ chatomoki.py
@@ -0,0 +1,14 @@
+print("こんにちは。chatomokiです")
+
+response=True
+
+while True:
+    user_words=input("何かはなしてみてね(qで終了):")    
+    if user_words=="q" or user_words== "ｑ":
+        break
+        
+    
+    print(user_words+"なんですね！")
+
+
+    response=True
